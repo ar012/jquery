616a435250938608abf8c447a65803f08ec3f162
Abdur Razzak <abdur.razzak@ctrends-software.com>
2019-02-11 01:04:33 +0600

    jquery updated

---
+++ js/index3.js
@@ -0,0 +1,12 @@
+// $('h1').html('Welcome');
+
+// $('h2').html('<img src="/img/sky.jpeg"/>');
+
+
+// $('.container').text("selftics")
+
+// $('.container').append("selftics")
+// $('.container').prepend("selftics")
+
+// $('.container').hide()
+
